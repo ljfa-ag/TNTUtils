83ee7a8e060af9822cd5847fff4e661088a39026
ljfa-ag <ljfa-ag@web.de>
2015-02-01 13:04:15 +0100

    Add some configs

--- src/main/java/ljfa/tntutils/Config.java
+++ src/main/java/ljfa/tntutils/Config.java
@@ -1,8 +1,11 @@
 package ljfa.tntutils;
 
 import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
 
 import net.minecraftforge.common.config.Configuration;
+import scala.actors.threadpool.Arrays;
 import cpw.mods.fml.client.event.ConfigChangedEvent;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
@@ -14,6 +17,8 @@ public class Config {
     
     public static boolean replaceTNT;
     public static boolean explosionCommand;
+    public static boolean spareTileEntities;
+    public static Set<String> blacklist;
     
     public static void loadConfig(File file) {
         if(conf == null)
@@ -28,6 +33,10 @@ public class Config {
     public static void loadValues() {
         replaceTNT = conf.getBoolean("preventChainExplosions", CATEGORY_GENERAL, true, "Prevent explosions from triggering TNT");
         explosionCommand = conf.getBoolean("addExplosionCommand", CATEGORY_GENERAL, true, "Adds the \"/explosion\" command");
+        spareTileEntities = conf.getBoolean("spareTileEntites", CATEGORY_GENERAL, false, "Makes explosions not destroy tile entities");
+        blacklist = new HashSet<String>(Arrays.asList(
+            conf.getStringList("destructionBlacklist", CATEGORY_GENERAL, new String[0], "A list of blocks that will never be destroyed by explosions")
+            ));
         //----------------
         if(conf.hasChanged())
             conf.save();
