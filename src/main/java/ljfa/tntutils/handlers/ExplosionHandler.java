309cbf16b986df024df473d82054f92857ab5150
ljfa-ag <ljfa-ag@web.de>
2015-02-01 11:21:31 +0100

    Add preliminary implementation

--- src/main/java/ljfa/tntutils/handlers/ExplosionHandler.java
+++ src/main/java/ljfa/tntutils/handlers/ExplosionHandler.java
@@ -1,5 +1,10 @@
 package ljfa.tntutils.handlers;
 
+import java.util.List;
+import java.util.function.Predicate;
+
+import net.minecraft.block.Block;
+import net.minecraft.world.ChunkPosition;
 import net.minecraftforge.event.world.ExplosionEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
@@ -10,7 +15,17 @@ public class ExplosionHandler {
     }
     
     @SubscribeEvent
-    public void onExplosionDetonate(ExplosionEvent.Detonate event) {
-        
+    public void onExplosionDetonate(final ExplosionEvent.Detonate event) {
+        if(event.world.isRemote)
+            return;
+        List<ChunkPosition> blocks = event.getAffectedBlocks();
+        blocks.removeIf(new Predicate<ChunkPosition>() {
+            @Override
+            public boolean test(ChunkPosition pos) {
+                Block block = event.world.getBlock(pos.chunkPosX, pos.chunkPosY, pos.chunkPosZ);
+                int meta = event.world.getBlockMetadata(pos.chunkPosX, pos.chunkPosY, pos.chunkPosZ);
+                return block.hasTileEntity(meta);
+            }
+        });
     }
 }
