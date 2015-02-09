7c1a78fcc938be28eafa54f8a765978e53c31b6b
ljfa-ag <ljfa-ag@web.de>
2015-02-09 13:02:54 +0100

    Add TransformerExclusions

--- src/main/java/ljfa/tntutils/coremod/TntuPlugin.java
+++ src/main/java/ljfa/tntutils/coremod/TntuPlugin.java
@@ -6,10 +6,12 @@ import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin.MCVersion;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin.Name;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin.SortingIndex;
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin.TransformerExclusions;
 
 @Name("TNTUtilities Core")
 @MCVersion("1.7.10")
 @SortingIndex(1100)
+@TransformerExclusions("ljfa.tntutils")
 public class TntuPlugin implements IFMLLoadingPlugin {
     @Override
     public String[] getASMTransformerClass() {
