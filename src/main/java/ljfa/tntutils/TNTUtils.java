ecdc2756d2e0a696492518c233d4e0ca0c21e81a
ljfa-ag <ljfa-ag@web.de>
2015-02-09 16:39:19 +0100

    Add dependencies

--- src/main/java/ljfa/tntutils/TNTUtils.java
+++ src/main/java/ljfa/tntutils/TNTUtils.java
@@ -13,7 +13,7 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartedEvent;
 
 @Mod(modid = Reference.MODID, name = Reference.MODNAME, version = Reference.VERSION, acceptableRemoteVersions = "*",
-    guiFactory = Reference.GUI_FACTORY_CLASS)
+    guiFactory = Reference.GUI_FACTORY_CLASS, dependencies = "required-after:tnt_utilities_core")
 public class TNTUtils {
     @Mod.Instance(Reference.MODID)
     public static TNTUtils instance;
