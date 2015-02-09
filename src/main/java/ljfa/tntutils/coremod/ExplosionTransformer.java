d62dd246cec6f32b2daae2b8192998a494ecb27d
ljfa-ag <ljfa-ag@web.de>
2015-02-09 13:16:19 +0100

    Fix circularity derp

--- src/main/java/ljfa/tntutils/coremod/ExplosionTransformer.java
+++ src/main/java/ljfa/tntutils/coremod/ExplosionTransformer.java
@@ -1,5 +1,8 @@
 package ljfa.tntutils.coremod;
 
+import org.apache.logging.log4j.Level;
+
+import cpw.mods.fml.relauncher.FMLRelaunchLog;
 import ljfa.tntutils.util.LogHelper;
 import net.minecraft.launchwrapper.IClassTransformer;
 
@@ -7,7 +10,7 @@ public class ExplosionTransformer implements IClassTransformer {
 
     @Override
     public byte[] transform(String name, String transformedName, byte[] basicClass) {
-        LogHelper.info("About to transform class %s", name);
+        FMLRelaunchLog.log("TntUtils-Core", Level.INFO, "About to transform class %s", name);
         return basicClass;
     }
 
