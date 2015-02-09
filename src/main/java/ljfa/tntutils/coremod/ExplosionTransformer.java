7d3361133157fa8cf90bfec91c90e9166453a5c1
ljfa-ag <ljfa-ag@web.de>
2015-02-09 13:37:08 +0100

    Add basics for transformations

--- src/main/java/ljfa/tntutils/coremod/ExplosionTransformer.java
+++ src/main/java/ljfa/tntutils/coremod/ExplosionTransformer.java
@@ -1,17 +1,47 @@
 package ljfa.tntutils.coremod;
 
-import org.apache.logging.log4j.Level;
+import java.util.Iterator;
 
-import cpw.mods.fml.relauncher.FMLRelaunchLog;
-import ljfa.tntutils.util.LogHelper;
 import net.minecraft.launchwrapper.IClassTransformer;
 
-public class ExplosionTransformer implements IClassTransformer {
+import org.apache.logging.log4j.Level;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.TypeInsnNode;
 
+import cpw.mods.fml.relauncher.FMLRelaunchLog;
+
+public class ExplosionTransformer implements IClassTransformer {
     @Override
     public byte[] transform(String name, String transformedName, byte[] basicClass) {
-        FMLRelaunchLog.log("TntUtils-Core", Level.INFO, "About to transform class %s", name);
-        return basicClass;
+        //ASM manipulation stuff
+        ClassNode classNode = new ClassNode();
+        ClassReader classReader = new ClassReader(basicClass);
+        classReader.accept(classNode, 0);
+        
+        for(MethodNode mn: classNode.methods) {
+            patchMethod(mn);
+        }
+        
+        //Write class
+        ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES);
+        classNode.accept(writer);
+        return writer.toByteArray();
     }
 
+    private void patchMethod(MethodNode mn) {
+        //Loop through the instructions of the method
+        Iterator<AbstractInsnNode> it = mn.instructions.iterator();
+        while(it.hasNext()) {
+            AbstractInsnNode currentNode = it.next();
+            
+            if(currentNode.getOpcode() == Opcodes.NEW) {
+                FMLRelaunchLog.log("TNTUtils Core", Level.INFO, "%s", ((TypeInsnNode)currentNode).desc);
+            }
+        }
+    }
 }
