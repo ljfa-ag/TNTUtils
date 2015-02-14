aaaf9c3ca76b06e715ddc6145234e050fa41a81a
ljfa-ag <ljfa-ag@web.de>
2015-02-14 12:37:03 +0100

    Outsource replaced TNT into its own class

--- src/main/java/ljfa/tntutils/blocks/ModBlocks.java
+++ src/main/java/ljfa/tntutils/blocks/ModBlocks.java
@@ -1,24 +1,11 @@
 package ljfa.tntutils.blocks;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockTNT;
-import net.minecraft.world.Explosion;
-import net.minecraft.world.World;
 
 public class ModBlocks {
     public static Block replaced_tnt;
     
     public static void preInit() {
-        replaced_tnt = new BlockTNT() {
-            @Override
-            public void onBlockDestroyedByExplosion(World world, int x, int y, int z, Explosion expl) {
-                //do nothing
-            }
-            
-            @Override
-            public boolean canDropFromExplosion(Explosion expl) {
-                return true;
-            };
-        }.setHardness(0.0F).setStepSound(Block.soundTypeGrass).setBlockName("tnt").setBlockTextureName("tnt");
+        replaced_tnt = new BlockReplacedTNT().setHardness(0.0F).setStepSound(Block.soundTypeGrass).setBlockName("tnt").setBlockTextureName("tnt");
     }
 }
