c8a2db7fffc478fa565fbb763f0f13f1b2772c7a
ljfa-ag <ljfa-ag@web.de>
2015-02-19 21:10:07 +0100

    Readd option for explosions to always drop
    This time with a threshold on the explosion size

--- src/main/java/ljfa/tntutils/asm/HooksExplosion.java
+++ src/main/java/ljfa/tntutils/asm/HooksExplosion.java
@@ -1,9 +1,15 @@
 package ljfa.tntutils.asm;
 
+import ljfa.tntutils.Config;
 import net.minecraft.world.Explosion;
 
 public class HooksExplosion {
+    public static final float alwaysDropThreshold = 7.0f;
+    
     public static float getDropChance(Explosion expl) {
-        return 1.0f / expl.explosionSize;
+        if(Config.alwaysDropItems && expl.explosionSize <= alwaysDropThreshold)
+            return 1.0f;
+        else
+            return 1.0f / expl.explosionSize;
     }
 }
