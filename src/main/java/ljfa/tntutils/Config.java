aa708a365dc80a8388d40d0bab5f5ba65d5d2513
ljfa-ag <ljfa-ag@web.de>
2015-02-20 11:16:45 +0100

    Rename to CAT_GENERAL

--- src/main/java/ljfa/tntutils/Config.java
+++ src/main/java/ljfa/tntutils/Config.java
@@ -16,7 +16,7 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 public class Config {
     public static Configuration conf;
 
-    public static final String CATEGORY_GENERAL = "general";
+    public static final String CAT_GENERAL = "general";
     
     public static boolean replaceTNT;
     public static boolean explosionCommand;
@@ -42,16 +42,16 @@ public class Config {
     }
     
     public static void loadValues() {
-        replaceTNT = conf.get(CATEGORY_GENERAL, "preventChainExplosions", false, "Prevents explosions from triggering TNT and thus disables chain explosions").setRequiresMcRestart(true).getBoolean();
-        explosionCommand = conf.get(CATEGORY_GENERAL, "addExplosionCommand", true, "Adds the \"/explosion\" command").setRequiresMcRestart(true).getBoolean();
-        spareTileEntities = conf.get(CATEGORY_GENERAL, "disableTileEntityDamage", false, "Makes explosions not destroy tile entities").getBoolean();
-        blacklistArray = conf.get(CATEGORY_GENERAL, "destructionBlacklist", new String[0], "A list of blocks that will never be destroyed by explosions").getStringList();
-        disableCreeperBlockDamage = conf.get(CATEGORY_GENERAL, "disableCreeperBlockDamage", false, "\"Environmentally Friendly Creepers\": Makes creepers not destroy blocks").getBoolean();
-        disableBlockDamage = conf.get(CATEGORY_GENERAL, "disableBlockDamage", false, "Disables all block damage from explosions").getBoolean();
-        disableEntityDamage = conf.get(CATEGORY_GENERAL, "disableEntityDamage", false, "Disables all entity damage from explosions").getBoolean();
-        disableNPCDamage = conf.get(CATEGORY_GENERAL, "disableNPCDamage", false, "No livings besides players get damage from explosions").getBoolean();
-        disableExplosions = conf.get(CATEGORY_GENERAL, "disableExplosions", false, "Entirely disables all effects from explosions").getBoolean();
-        dropChanceModifier = (float)conf.get(CATEGORY_GENERAL, "dropChanceIncrease", 0.0, "Increases the chance that explosions will drop destroyed blocks as items\n"
+        replaceTNT = conf.get(CAT_GENERAL, "preventChainExplosions", false, "Prevents explosions from triggering TNT and thus disables chain explosions").setRequiresMcRestart(true).getBoolean();
+        explosionCommand = conf.get(CAT_GENERAL, "addExplosionCommand", true, "Adds the \"/explosion\" command").setRequiresMcRestart(true).getBoolean();
+        spareTileEntities = conf.get(CAT_GENERAL, "disableTileEntityDamage", false, "Makes explosions not destroy tile entities").getBoolean();
+        blacklistArray = conf.get(CAT_GENERAL, "destructionBlacklist", new String[0], "A list of blocks that will never be destroyed by explosions").getStringList();
+        disableCreeperBlockDamage = conf.get(CAT_GENERAL, "disableCreeperBlockDamage", false, "\"Environmentally Friendly Creepers\": Makes creepers not destroy blocks").getBoolean();
+        disableBlockDamage = conf.get(CAT_GENERAL, "disableBlockDamage", false, "Disables all block damage from explosions").getBoolean();
+        disableEntityDamage = conf.get(CAT_GENERAL, "disableEntityDamage", false, "Disables all entity damage from explosions").getBoolean();
+        disableNPCDamage = conf.get(CAT_GENERAL, "disableNPCDamage", false, "No livings besides players get damage from explosions").getBoolean();
+        disableExplosions = conf.get(CAT_GENERAL, "disableExplosions", false, "Entirely disables all effects from explosions").getBoolean();
+        dropChanceModifier = (float)conf.get(CAT_GENERAL, "dropChanceIncrease", 0.0, "Increases the chance that explosions will drop destroyed blocks as items\n"
                 + "0 = Vanilla behavior, 1 = always drop items.\n"
                 + "This option will only affect explosions of size <= 7 because a large number of dropped items can cause lag.", 0.0, 1.0).getDouble();
         //----------------
