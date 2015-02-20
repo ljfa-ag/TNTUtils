d942c8fcc9e0102e497570a2441192bf14eada70
ljfa-ag <ljfa-ag@web.de>
2015-02-20 12:56:19 +0100

    Increase threshold

--- src/main/java/ljfa/tntutils/asm/HooksExplosion.java
+++ src/main/java/ljfa/tntutils/asm/HooksExplosion.java
@@ -4,7 +4,7 @@ import ljfa.tntutils.Config;
 import net.minecraft.world.Explosion;
 
 public class HooksExplosion {
-    public static final float alwaysDropThreshold = 7.0f;
+    public static final float alwaysDropThreshold = 10.0f;
     
     public static float getDropChance(Explosion expl) {
         float baseChance = 1.0f / expl.explosionSize;
