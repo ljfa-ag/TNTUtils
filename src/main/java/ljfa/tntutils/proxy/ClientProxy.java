9f371c0c58158d769462c68de13d97387bf130c3
ljfa-ag <ljfa-ag@web.de>
2015-05-23 17:56:04 +0200

    Comment out unneeded stuff

--- src/main/java/ljfa/tntutils/proxy/ClientProxy.java
+++ src/main/java/ljfa/tntutils/proxy/ClientProxy.java
@@ -7,14 +7,14 @@ import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
 public class ClientProxy extends CommonProxy {
-    public ClientProxy() {
+    /*public ClientProxy() {
         mc = Minecraft.getMinecraft();
-    }
+    }*/
     
     /*@Override
     public void init(FMLInitializationEvent event) {
         super.init(event);
     }*/
     
-    private Minecraft mc;
+    //private Minecraft mc;
 }
