ded6019cd07b13b87fe43c0db8da4fa86778895d
ljfa-ag <ljfa-ag@web.de>
2015-05-23 17:48:53 +0200

    Allow negative values for dropChanceIncrease (cherry picked from commit 6ff5ffc1931e02ae4aa5757e55f752b4a25ec1ea)

--- src/main/java/ljfa/tntutils/asm/HooksExplosion.java
+++ src/main/java/ljfa/tntutils/asm/HooksExplosion.java
@@ -8,9 +8,14 @@ public class HooksExplosion {
     
     public static float getDropChance(Explosion expl) {
         float baseChance = 1.0f / expl.explosionSize;
-        if(expl.explosionSize <= alwaysDropThreshold)
-            return baseChance + Config.dropChanceModifier * (1.0f - baseChance);
-        else
+        if(Config.dropChanceModifier == 0.0f)
             return baseChance;
+        else if(Config.dropChanceModifier > 0.0f) {
+            if(expl.explosionSize <= alwaysDropThreshold)
+                return baseChance + Config.dropChanceModifier * (1.0f - baseChance);
+            else
+                return baseChance;
+        } else
+            return (1.0f + Config.dropChanceModifier) * baseChance;
     }
 }
