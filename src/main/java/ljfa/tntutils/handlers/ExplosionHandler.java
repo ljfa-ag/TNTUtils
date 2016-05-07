23519b242b6cee5f7ea94c6367b3e42ad192d808
ljfa-ag <ljfa-ag@users.noreply.github.com>
2016-05-07 12:14:34 +0200

    Implement preventChainExpl differently
    The least hacky solution I guess
    
    Conflicts:
    	src/main/java/ljfa/tntutils/handlers/ExplosionHandler.java

--- src/main/java/ljfa/tntutils/handlers/ExplosionHandler.java
+++ src/main/java/ljfa/tntutils/handlers/ExplosionHandler.java
@@ -2,9 +2,11 @@ package ljfa.tntutils.handlers;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import ljfa.tntutils.Config;
+import ljfa.tntutils.asm.HooksExplosion;
 import ljfa.tntutils.util.ListHelper;
 import ljfa.tntutils.util.Predicate;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockTNT;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItem;
@@ -12,6 +14,7 @@ import net.minecraft.entity.item.EntityMinecartTNT;
 import net.minecraft.entity.monster.EntityCreeper;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.ChunkPosition;
+import net.minecraft.world.World;
 import net.minecraftforge.event.world.ExplosionEvent;
 
 public class ExplosionHandler {
@@ -25,21 +28,38 @@ public class ExplosionHandler {
 
     @SubscribeEvent
     public void onExplosionDetonate(final ExplosionEvent.Detonate event) {
-        if(event.world.isRemote)
+        final World world = event.world;
+        if(world.isRemote)
             return;
+        
         //Block damage
         if(Config.disableBlockDamage || (Config.disableCreeperBlockDamage && event.explosion.exploder instanceof EntityCreeper))
-            event.explosion.affectedBlockPositions.clear();
-        else if(Config.spareTileEntities || Config.blacklistActive) {
-            //Remove blacklisted blocks and tile entities (if configured) from the list
-            ListHelper.removeIf(event.getAffectedBlocks(), new Predicate<ChunkPosition>() {
-                @Override
-                public boolean test(ChunkPosition pos) {
-                    Block block = event.world.getBlock(pos.chunkPosX, pos.chunkPosY, pos.chunkPosZ);
-                    int meta = event.world.getBlockMetadata(pos.chunkPosX, pos.chunkPosY, pos.chunkPosZ);
-                    return shouldBePreserved(block, meta);
+            event.getAffectedBlocks().clear();
+        else {
+            if(Config.spareTileEntities || Config.blacklistActive) {
+                //Remove blacklisted blocks and tile entities (if configured) from the list
+                ListHelper.removeIf(event.getAffectedBlocks(), new Predicate<ChunkPosition>() {
+                    @Override
+                    public boolean test(ChunkPosition pos) {
+                        Block block = world.getBlock(pos.chunkPosX, pos.chunkPosY, pos.chunkPosZ);
+                        int meta = world.getBlockMetadata(pos.chunkPosX, pos.chunkPosY, pos.chunkPosZ);
+                        return shouldBePreserved(block, meta);
+                    }
+                });
+            }
+            
+            if(Config.preventChainExpl) {
+                //Manually remove TNT blocks from the world before the actual explosion destroys them
+                //(which would cause a chain explosion)
+                for(ChunkPosition pos: event.getAffectedBlocks()) {
+                    int x = pos.chunkPosX, y = pos.chunkPosY, z = pos.chunkPosZ;
+                    Block block = world.getBlock(x, y, z);
+                    if(block instanceof BlockTNT) {
+                        block.dropBlockAsItemWithChance(world, x, y, z, world.getBlockMetadata(x, y, z), HooksExplosion.getDropChance(event.explosion), 0);
+                        world.setBlockToAir(x, y, z);
+                    }
                 }
-            });
+            }
         }
         
         //Entity damage
