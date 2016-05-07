b4d6256a9c625205de0c57f1e901c062cd5adddb
ljfa-ag <ljfa-ag@users.noreply.github.com>
2016-05-07 12:50:22 +0200

    Organize imports

--- src/main/java/ljfa/tntutils/proxy/ClientProxy.java
+++ src/main/java/ljfa/tntutils/proxy/ClientProxy.java
@@ -1,11 +1,5 @@
 package ljfa.tntutils.proxy;
 
-import ljfa.tntutils.TNTUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.resources.model.ModelResourceLocation;
-import net.minecraft.item.Item;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-
 public class ClientProxy extends CommonProxy {
     /*public ClientProxy() {
         mc = Minecraft.getMinecraft();
