8afe2b3398c4e8aad2699041cc3dadfe67ae0672
ljfa-ag <ljfa-ag@users.noreply.github.com>
2016-06-08 13:55:32 +0200

    Change updateJSON to point to the version branch

--- src/main/java/ljfa/tntutils/TNTUtils.java
+++ src/main/java/ljfa/tntutils/TNTUtils.java
@@ -15,7 +15,7 @@ import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 
 @Mod(modid = Reference.MODID, name = Reference.MODNAME, version = Reference.VERSION, acceptableRemoteVersions = "*",
     guiFactory = Reference.GUI_FACTORY_CLASS, dependencies = "required-after:tnt_utilities_core",
-    acceptedMinecraftVersions = "[1.9,)", updateJSON = Reference.UPDATE_JSON)
+    acceptedMinecraftVersions = "[1.9]", updateJSON = Reference.UPDATE_JSON)
 public class TNTUtils {
     @Mod.Instance(Reference.MODID)
     public static TNTUtils instance;
