916d9f3d34ec931ed768d10c4f84772832a69376
ljfa-ag <ljfa-ag@users.noreply.github.com>
2016-06-08 13:37:41 +0200

    Change updateJSON to point to the version branch
    Also change acceptedMinecraftVersions

--- src/main/java/ljfa/tntutils/Reference.java
+++ src/main/java/ljfa/tntutils/Reference.java
@@ -7,5 +7,5 @@ public class Reference {
     public static final String CLIENT_PROXY_CLASS = "ljfa.tntutils.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "ljfa.tntutils.proxy.CommonProxy";
     public static final String GUI_FACTORY_CLASS = "ljfa.tntutils.gui.TntuGuiFactory";
-    public static final String UPDATE_JSON = "https://raw.githubusercontent.com/ljfa-ag/TNTUtils/master/version.json";
+    public static final String UPDATE_JSON = "https://raw.githubusercontent.com/ljfa-ag/TNTUtils/version/version.json";
 }
