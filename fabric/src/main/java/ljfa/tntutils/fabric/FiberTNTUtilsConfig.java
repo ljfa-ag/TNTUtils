bb309eee0f47b887eb77e4f037615dd8a6e9ec70
ljfa <ljfa-ag@users.noreply.github.com>
2025-01-05 13:23:34 +0100

    Load and save the Fiber config

--- fabric/src/main/java/ljfa/tntutils/fabric/FiberTNTUtilsConfig.java
+++ fabric/src/main/java/ljfa/tntutils/fabric/FiberTNTUtilsConfig.java
@@ -1,9 +1,21 @@
 package ljfa.tntutils.fabric;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.StandardOpenOption;
+
+import io.github.fablabsmc.fablabs.api.fiber.v1.exception.ValueDeserializationException;
 import io.github.fablabsmc.fablabs.api.fiber.v1.schema.type.derived.ConfigTypes;
+import io.github.fablabsmc.fablabs.api.fiber.v1.serialization.FiberSerialization;
+import io.github.fablabsmc.fablabs.api.fiber.v1.serialization.JanksonValueSerializer;
 import io.github.fablabsmc.fablabs.api.fiber.v1.tree.ConfigTree;
 import io.github.fablabsmc.fablabs.api.fiber.v1.tree.PropertyMirror;
+import ljfa.tntutils.TNTUtils;
 import ljfa.tntutils.TNTUtilsConfig;
+import net.fabricmc.loader.api.FabricLoader;
 
 public class FiberTNTUtilsConfig {
 	public static class Common implements TNTUtilsConfig {
@@ -27,5 +39,26 @@ public class FiberTNTUtilsConfig {
 
 	public static void init() {
 		var configTree = COMMON.buildConfig();
+
+		var configFile = FabricLoader.getInstance().getConfigDir().resolve("tntutils.json5");
+		var serializer = new JanksonValueSerializer(false);
+
+		//try reading the config file
+		try(var reader = new BufferedInputStream(Files.newInputStream(configFile))) {
+			FiberSerialization.deserialize(configTree, reader, serializer);
+		}
+		catch (NoSuchFileException ignored) {}
+		catch (IOException | ValueDeserializationException e) {
+			TNTUtils.logger.error("Error reading config file", e);
+		}
+
+		//write the config file
+		//TODO: Can we avoid writing the config from scratch if nothing has changed?
+		try(var writer = new BufferedOutputStream(Files.newOutputStream(configFile, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING))) {
+			FiberSerialization.serialize(configTree, writer, serializer);
+		}
+		catch (IOException e) {
+			TNTUtils.logger.error("Error writing config file", e);
+		}
 	}
 }
