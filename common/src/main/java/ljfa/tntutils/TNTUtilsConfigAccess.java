1fcd00ffd2a1c2ff8001137da34b19bd2d1b2ce8
ljfa <ljfa-ag@users.noreply.github.com>
2025-02-09 19:35:57 +0100

    Rename _KEY constants to _TRANSLATION

--- common/src/main/java/ljfa/tntutils/TNTUtilsConfigAccess.java
+++ common/src/main/java/ljfa/tntutils/TNTUtilsConfigAccess.java
@@ -10,10 +10,10 @@ public interface TNTUtilsConfigAccess {
 
 	static final String GENERAL_COMMENT = "General options";
 	static final String ADD_EXPLODE_COMMAND_COMMENT = "Adds the '/explode' command";
-	static final String ADD_EXPLODE_COMMAND_KEY = "tntutils.config.addExplodeCommand";
+	static final String ADD_EXPLODE_COMMAND_TRANSLATION = "tntutils.config.addExplodeCommand";
 	static final boolean ADD_EXPLODE_COMMAND_DEFAULT = true;
 	static final String SIZE_MULTIPLIER_COMMENT = "Multiplies the size of all explosions by this value";
-	static final String SIZE_MULTIPLIER_KEY = "tntutils.config.sizeMultiplier";
+	static final String SIZE_MULTIPLIER_TRANSLATION = "tntutils.config.sizeMultiplier";
 	static final float SIZE_MULTIPLIER_MIN = 0.0f;
 	static final float SIZE_MULTIPLIER_MAX = 50.0f;
 	static final float SIZE_MULTIPLIER_DEFAULT = 1.0f;
@@ -26,7 +26,7 @@ public interface TNTUtilsConfigAccess {
 			Will not affect explosions without block drop decay (i.e. TNT explosions by default).
 			To get a drop chance of 100%, set the game rules "mobExplosionDropDecay", "tntExplosionDropDecay" and
 			"blockExplosionDropDecay" to "false".""";
-	static final String DROP_CHANCE_MULTIPLIER_KEY = "tntutils.config.dropChanceMultiplier";
+	static final String DROP_CHANCE_MULTIPLIER_TRANSLATION = "tntutils.config.dropChanceMultiplier";
 	static final float DROP_CHANCE_MULTIPLIER_MIN = 0.0f;
 	static final float DROP_CHANCE_MULTIPLIER_DEFAULT = 1.0f;
 }
--- neoforge/src/main/java/ljfa/tntutils/neoforge/NeoforgeTNTUtilsConfig.java
+++ neoforge/src/main/java/ljfa/tntutils/neoforge/NeoforgeTNTUtilsConfig.java
@@ -16,18 +16,18 @@ public class NeoforgeTNTUtilsConfig {
 			builder.comment(GENERAL_COMMENT).push("general");
 			addExplodeCommand = builder
 					.comment(ADD_EXPLODE_COMMAND_COMMENT)
-					.translation(ADD_EXPLODE_COMMAND_KEY)
+					.translation(ADD_EXPLODE_COMMAND_TRANSLATION)
 					.gameRestart()
 					.define("addExplodeCommand", ADD_EXPLODE_COMMAND_DEFAULT);
 			sizeMultiplier = builder
 					.comment(SIZE_MULTIPLIER_COMMENT)
-					.translation(SIZE_MULTIPLIER_KEY)
+					.translation(SIZE_MULTIPLIER_TRANSLATION)
 					.defineInRange("sizeMultiplier", SIZE_MULTIPLIER_DEFAULT, SIZE_MULTIPLIER_MIN, SIZE_MULTIPLIER_MAX);
 
 			builder.pop().comment(BLOCK_DAMAGE_COMMENT).push("blockDamage");
 			dropChanceMultiplier = builder
 					.comment(DROP_CHANCE_MULTIPLIER_COMMENT)
-					.translation(DROP_CHANCE_MULTIPLIER_KEY)
+					.translation(DROP_CHANCE_MULTIPLIER_TRANSLATION)
 					.defineInRange("dropChanceMultiplier", DROP_CHANCE_MULTIPLIER_DEFAULT, DROP_CHANCE_MULTIPLIER_MIN, Double.POSITIVE_INFINITY);
 		}
 
