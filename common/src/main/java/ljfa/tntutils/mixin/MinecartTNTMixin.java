35a848e95f87096a5be32c289fb1cdb97da25808
ljfa <ljfa-ag@users.noreply.github.com>
2025-03-26 23:40:53 +0100

    Implement disableTNT for TNT minecarts

---
+++ common/src/main/java/ljfa/tntutils/mixin/MinecartTNTMixin.java
@@ -0,0 +1,48 @@
+package ljfa.tntutils.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import ljfa.tntutils.TNTUtils;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.vehicle.AbstractMinecart;
+import net.minecraft.world.entity.vehicle.MinecartTNT;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.Level;
+
+@Mixin(MinecartTNT.class)
+public abstract class MinecartTNTMixin extends AbstractMinecart {
+	@Shadow(prefix = "shadow$") //use prefix to silence warning about not overriding the package-private method VehicleEntity.getDropItem()
+	protected abstract Item shadow$getDropItem();
+
+	//Disarm the TNT minecart as soon as it is ignited
+	@Inject(method = "primeFuse",
+			at = @At("HEAD"),
+			cancellable = true)
+	private void onPrimeFuse(CallbackInfo ci) {
+		if(TNTUtils.config().disableTNT()) {
+			if(!this.level().isClientSide())
+				this.destroy(this.shadow$getDropItem());
+			ci.cancel();
+		}
+	}
+
+	//Make sure the TNT minecart also doesn't explode when falling down or being shot by a flaming arrow
+	@Inject(method = "explode(Lnet/minecraft/world/damagesource/DamageSource;D)V",
+			at = @At("HEAD"),
+			cancellable = true)
+	private void onExplode(CallbackInfo ci) {
+		if(TNTUtils.config().disableTNT()) {
+			if(!this.level().isClientSide())
+				this.destroy(this.shadow$getDropItem());
+			ci.cancel();
+		}
+	}
+
+	private MinecartTNTMixin(EntityType<?> entityType, Level level) {
+		super(entityType, level);
+	}
+}
