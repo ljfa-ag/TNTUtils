ef3e5b160054eae6f1d5d6ca92e54e3ec86528f0
ljfa <ljfa-ag@users.noreply.github.com>
2025-05-03 12:56:24 +0200

    Implement modifyExplosionResistances for Fabric

--- fabric/src/main/java/ljfa/tntutils/fabric/FiberTNTUtilsConfig.java
+++ fabric/src/main/java/ljfa/tntutils/fabric/FiberTNTUtilsConfig.java
@@ -8,6 +8,8 @@ import java.nio.file.NoSuchFileException;
 import java.nio.file.StandardOpenOption;
 import java.util.Map;
 
+import com.google.common.collect.ImmutableMap;
+
 import io.github.fablabsmc.fablabs.api.fiber.v1.exception.ValueDeserializationException;
 import io.github.fablabsmc.fablabs.api.fiber.v1.schema.type.derived.ConfigTypes;
 import io.github.fablabsmc.fablabs.api.fiber.v1.serialization.FiberSerialization;
@@ -17,6 +19,9 @@ import io.github.fablabsmc.fablabs.api.fiber.v1.tree.PropertyMirror;
 import ljfa.tntutils.TNTUtils;
 import ljfa.tntutils.TNTUtilsConfigAccess;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Block;
 
 public class FiberTNTUtilsConfig {
 	public static class Common implements TNTUtilsConfigAccess {
@@ -32,6 +37,7 @@ public class FiberTNTUtilsConfig {
 		public final PropertyMirror<Boolean> spareBlockEntities = PropertyMirror.create(ConfigTypes.BOOLEAN);
 		public final PropertyMirror<Map<String, Float>> modifyExplosionResistances = PropertyMirror.create(
 				ConfigTypes.makeMap(ConfigTypes.STRING, ConfigTypes.FLOAT.withMinimum(0.0f)));
+		private Map<Block, Float> explosionResistanceMap;
 
 		public final PropertyMirror<Boolean> disableEntityDamage = PropertyMirror.create(ConfigTypes.BOOLEAN);
 		public final PropertyMirror<Boolean> disablePlayerDamage = PropertyMirror.create(ConfigTypes.BOOLEAN);
@@ -114,6 +120,24 @@ public class FiberTNTUtilsConfig {
 					.build();
 		}
 
+		private void createExplosionResistanceMap() {
+			TNTUtils.logger.debug("Creating explosion resistance map");
+			var configMap = modifyExplosionResistances.getValue();
+			var builder = ImmutableMap.<Block, Float>builderWithExpectedSize(configMap.size());
+			for(var entry : configMap.entrySet()) {
+				try {
+					var key = entry.getKey();
+					var block = BuiltInRegistries.BLOCK.getOptional(ResourceLocation.parse(key))
+							.orElseThrow(() -> new RuntimeException("Unknown block ID: \"" + key + "\""));
+					builder.put(block, entry.getValue());
+				}
+				catch(Exception e) {
+					TNTUtils.logger.error("Error reading the modifyExplosionResistances config value", e);
+				}
+			}
+			explosionResistanceMap = builder.buildOrThrow();
+		}
+
 		@Override
 		public boolean addExplodeCommand() {
 			return addExplodeCommand.getValue();
@@ -160,6 +184,11 @@ public class FiberTNTUtilsConfig {
 		}
 
 		@Override
+		public Map<Block, Float> explosionResistanceMap() {
+			return explosionResistanceMap;
+		}
+
+		@Override
 		public boolean disableEntityDamage() {
 			return disableEntityDamage.getValue();
 		}
@@ -196,6 +225,7 @@ public class FiberTNTUtilsConfig {
 		catch (IOException | ValueDeserializationException e) {
 			TNTUtils.logger.error("Error reading config file", e);
 		}
+		COMMON.createExplosionResistanceMap();
 
 		//write the config file
 		//TODO: Can we avoid writing the config from scratch if nothing has changed?
