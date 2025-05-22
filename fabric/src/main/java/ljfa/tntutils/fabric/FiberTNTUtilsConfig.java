d3cf68414fc1a3db23d4cab7918b4355379c9f8f
ljfa <ljfa-ag@users.noreply.github.com>
2025-05-22 20:27:59 +0200

    Reimplement modifyExplosionResistances for Fabric
    as Fabric doesn't have RegistryEntryAddedCallback.allEntries in 1.20
    
    Fix remaining errors for Fabric

--- fabric/src/main/java/ljfa/tntutils/fabric/FiberTNTUtilsConfig.java
+++ fabric/src/main/java/ljfa/tntutils/fabric/FiberTNTUtilsConfig.java
@@ -31,7 +31,6 @@ public class FiberTNTUtilsConfig {
         public final PropertyMirror<Float> dropChanceMultiplier = PropertyMirror.create(ConfigTypes.FLOAT);
         public final PropertyMirror<Boolean> disableBlockDamage = PropertyMirror.create(ConfigTypes.BOOLEAN);
         public final PropertyMirror<Boolean> disableCreeperBlockDamage = PropertyMirror.create(ConfigTypes.BOOLEAN);
-        public final PropertyMirror<Boolean> disableBlockTriggering = PropertyMirror.create(ConfigTypes.BOOLEAN);
         public final PropertyMirror<Boolean> spareBlockEntities = PropertyMirror.create(ConfigTypes.BOOLEAN);
         public final PropertyMirror<Map<String, Float>> modifyExplosionResistances = PropertyMirror.create(
                 ConfigTypes.makeMap(ConfigTypes.STRING, ConfigTypes.FLOAT.withMinimum(0.0f)));
@@ -83,10 +82,6 @@ public class FiberTNTUtilsConfig {
                     .beginValue("disableCreeperBlockDamage", ConfigTypes.BOOLEAN, DISABLE_CREEPER_BLOCK_DAMAGE_DEFAULT)
                     .withComment(DISABLE_CREEPER_BLOCK_DAMAGE_COMMENT)
                     .finishValue(disableCreeperBlockDamage::mirror)
-
-                    .beginValue("disableBlockTriggering", ConfigTypes.BOOLEAN, DISABLE_BLOCK_TRIGGERING_DEFAULT)
-                    .withComment(DISABLE_BLOCK_TRIGGERING_COMMENT)
-                    .finishValue(disableBlockTriggering::mirror)
 
                     .beginValue("spareBlockEntities", ConfigTypes.BOOLEAN, SPARE_BLOCK_ENTITIES_DEFAULT)
                     .withComment(SPARE_BLOCK_ENTITIES_COMMENT)
@@ -159,11 +154,6 @@ public class FiberTNTUtilsConfig {
         @Override
         public boolean disableCreeperBlockDamage() {
             return disableCreeperBlockDamage.getValue();
-        }
-
-        @Override
-        public boolean disableBlockTriggering() {
-            return disableBlockTriggering.getValue();
         }
 
         @Override
--- fabric/src/main/java/ljfa/tntutils/fabric/TNTUtilsFabricEntry.java
+++ fabric/src/main/java/ljfa/tntutils/fabric/TNTUtilsFabricEntry.java
@@ -4,12 +4,11 @@ import java.util.HashMap;
 
 import ljfa.tntutils.TNTUtils;
 import ljfa.tntutils.command.ExplodeCommand;
+import ljfa.tntutils.mixin.BlockBehaviourAccessor;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.registry.RegistryEntryAddedCallback;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 
@@ -25,33 +24,39 @@ public class TNTUtilsFabricEntry implements ModInitializer {
     }
 
     private void handleModifyExplosionResistances() {
-        /*
-         * Since Fabric deliberately doesn't have a concept of a stage where all mod loading is finished, we can't be
-         * sure when all blocks are registered. Rather, we need to use the RegistryEntryAddedCallback.
-         * The drawback is that we can't check at this point that the block IDs in the config are spelled correctly.
-         */
         var stringMap = FiberTNTUtilsConfig.COMMON.modifyExplosionResistances.getValue();
         if(stringMap.isEmpty())
             return;
 
-        //parse the Strings in the config into ResourceKeys
-        var resKeyMap = new HashMap<ResourceKey<Block>, Float>(stringMap.size());
+        //Not all mods' blocks might already be registered at this point, so we query the registry and store unknown IDs in this map.
+        var unknownBlocks = new HashMap<ResourceLocation, Float>();
         for(var entry : stringMap.entrySet()) {
             try {
-                var resKey = ResourceKey.create(Registries.BLOCK, ResourceLocation.parse(entry.getKey()));
-                resKeyMap.put(resKey, entry.getValue());
+                var id = new ResourceLocation(entry.getKey());
+                var block = BuiltInRegistries.BLOCK.get(id);
+                if(block != null)
+                    setExplosionResistance(id, block, entry.getValue());
+                else
+                    unknownBlocks.put(id, entry.getValue());
             }
             catch(Exception e) {
                 TNTUtils.logger.error("Error reading the modifyExplosionResistances config value: " + e.getMessage());
             }
         }
 
-        RegistryEntryAddedCallback.allEntries(BuiltInRegistries.BLOCK, holder -> {
-            Float value = resKeyMap.get(holder.key());
-            if(value != null) {
-                holder.value().explosionResistance = value;
-                TNTUtils.logger.debug("Changed explosion resistance for " + holder.key().location());
-            }
-        });
+        //install a callback for the unknown blocks
+        if(!unknownBlocks.isEmpty()) {
+            TNTUtils.logger.debug("Installing callback to change explosion resistance of " + unknownBlocks.size() + " blocks");
+            RegistryEntryAddedCallback.event(BuiltInRegistries.BLOCK).register((rawId, id, block) -> {
+                Float value = unknownBlocks.get(id);
+                if(value != null)
+                    setExplosionResistance(id, block, value);
+            });
+        }
+    }
+
+    private void setExplosionResistance(ResourceLocation id, Block block, float value) {
+        ((BlockBehaviourAccessor) block).setExplosionResistance(value);
+        TNTUtils.logger.debug("Changed explosion resistance for " + id);
     }
 }
