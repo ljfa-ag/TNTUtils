4a4806748b57536d42bf8a7e956510472fbb0e9d
ljfa <ljfa-ag@users.noreply.github.com>
2025-05-23 13:21:10 +0200

    Add missing @Mutable annotations to accessor mixins

--- common/src/main/java/ljfa/tntutils/mixin/ExplosionAccessor.java
+++ common/src/main/java/ljfa/tntutils/mixin/ExplosionAccessor.java
@@ -1,6 +1,7 @@
 package ljfa.tntutils.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 import net.minecraft.world.level.Explosion;
@@ -11,10 +12,12 @@ public interface ExplosionAccessor {
     @Accessor
     float getRadius();
     @Accessor
+    @Mutable
     void setRadius(float radius);
 
     @Accessor
     ExplosionDamageCalculator getDamageCalculator();
     @Accessor
+    @Mutable
     void setDamageCalculator(ExplosionDamageCalculator damageCalculator);
 }
