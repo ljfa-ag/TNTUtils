81c30a5f4e80907fc711a822784dd94f8f86bd6f
ljfa <ljfa-ag@users.noreply.github.com>
2025-05-25 19:47:31 +0200

    Reimplement MinecartTNTMixin

--- common/src/main/java/ljfa/tntutils/mixin/MinecartTNTMixin.java
+++ common/src/main/java/ljfa/tntutils/mixin/MinecartTNTMixin.java
@@ -1,30 +1,27 @@
 package ljfa.tntutils.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import ljfa.tntutils.TNTUtils;
+import net.minecraft.tags.DamageTypeTags;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.vehicle.AbstractMinecart;
 import net.minecraft.world.entity.vehicle.MinecartTNT;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
 
 @Mixin(MinecartTNT.class)
 public abstract class MinecartTNTMixin extends AbstractMinecart {
-    @Shadow(prefix = "shadow$") //use prefix to silence warning about not overriding the package-private method VehicleEntity.getDropItem()
-    protected abstract Item shadow$getDropItem();
-
     //Disarm the TNT minecart as soon as it is ignited
     @Inject(method = "primeFuse",
             at = @At("HEAD"),
             cancellable = true)
     private void onPrimeFuse(CallbackInfo ci) {
         if(TNTUtils.config().disableTNT() && !this.level().isClientSide()) {
-            //this.destroy(this.shadow$getDropItem());
+            super.destroy(damageSources().generic());
             ci.cancel();
         }
     }
@@ -35,19 +32,20 @@ public abstract class MinecartTNTMixin extends AbstractMinecart {
             cancellable = true)
     private void onExplode(CallbackInfo ci) {
         if(TNTUtils.config().disableTNT() && !this.level().isClientSide()) {
-            //this.destroy(this.shadow$getDropItem());
+            super.destroy(damageSources().generic());
             ci.cancel();
         }
     }
 
-    //FIXME: This method does not exist in 1.20, need to implement this differently
-    /*@ModifyReturnValue(method = "damageSourceIgnitesTnt", at = @At("RETURN"))
-    private static boolean changeDamageSourcesIgnitesTNT(boolean original, DamageSource source) {
-        if(TNTUtils.config().preventChainExplosions())
-            return original && !source.is(DamageTypeTags.IS_EXPLOSION);
-        else
-            return original;
-    }*/
+    @Inject(method = "destroy",
+            at = @At("HEAD"),
+            cancellable = true)
+    private void onDestroy(DamageSource source, CallbackInfo ci) {
+        if(TNTUtils.config().preventChainExplosions() && source.is(DamageTypeTags.IS_EXPLOSION)) {
+            super.destroy(source);
+            ci.cancel();
+        }
+    }
 
     private MinecartTNTMixin(EntityType<?> entityType, Level level) {
         super(entityType, level);
