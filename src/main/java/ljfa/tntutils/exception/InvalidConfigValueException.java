10cc827fd59f7da0ee81006e4e3af296d9c72bf3
ljfa <ljfa-ag@users.noreply.github.com>
2025-07-03 19:36:19 +0200

    Remove unused Utils class
    Add serialVersionUID to InvalidConfigValueException

--- src/main/java/ljfa/tntutils/exception/InvalidConfigValueException.java
+++ src/main/java/ljfa/tntutils/exception/InvalidConfigValueException.java
@@ -1,18 +1,20 @@
 package ljfa.tntutils.exception;
 
 public class InvalidConfigValueException extends RuntimeException {
+    private static final long serialVersionUID = -7206620619144191952L;
+
     public InvalidConfigValueException() {
-        
+
     }
-    
+
     public InvalidConfigValueException(String message) {
         super(message);
     }
-    
+
     public InvalidConfigValueException(Throwable cause) {
         super(cause);
     }
-    
+
     public InvalidConfigValueException(String message, Throwable cause) {
         super(message, cause);
     }
--- src/main/java/ljfa/tntutils/util/Utils.java
+++
@@ -1,7 +0,0 @@
-package ljfa.tntutils.util;
-
-import net.minecraft.launchwrapper.Launch;
-
-public class Utils {
-    public static final boolean deobfuscatedEnv = (Boolean)Launch.blackboard.get("fml.deobfuscatedEnvironment");
-}
